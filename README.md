b4ef9c9244b673e63590fee3467f48d3bd0f5146
Igor <mr.igor96@outlook.com>
2017-05-31 08:16:20 +0200

    init starterkit-cli

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# angular-2-rails-starterkit
+# Angular 2 Rails Starterkit
 quick start with angular 2 and rails api application
+
+<img src="https://raw.githubusercontent.com/igorbezsmertnyi/angular-2-rails-starterkit/master/src/logo.png" height="200px" />
+
+
+## Installtion
+```npm install angular-2-rails-starterkit -g```<br/>
+for linux ```sudo npm install angular-2-rails-starterkit -g```
+
+## Quick Start
+- **Install Angular-CLI**
+  ```npm install -g @angular/cli```<br/>
+  for linux ```sudo npm install -g @angular/cli```
+- **Generate Rails project**<br/>
+  ```rails new PROJECT_NEME --api```
+- **Install gems**<br/>
+  Add to **Gemfile**
+  > ```gem 'rack-rewrite'```<br/>
+  > ```gem 'rack-cors'```<br/>
+  > ```gem 'foreman'``` to ```group :development```<br/>
+  >then run ```bundle install```
+- **Inside project folder run starterkit**<br/>
+  ```starterkit -g```
+- **Strat Server**<br/>
+  ```bundle exec foreman start -f Procfile.dev```
+- **Generate Production**<br/>
+  ```bundle exec foreman start -f Procfile.prod```
+
+
+
+[LINK TO NPM PACKAGE](https://www.npmjs.com/package/angular-2-rails-starterkit)
--- starterkit-cli.js
+++ starterkit-cli.js
@@ -3,20 +3,120 @@ const path = require('path')
 const Repository = require('git-cli').Repository
 const fs = require('fs')
 const Program = require('commander')
+const Promise = require('bluebird')
+const cmd = require('node-cmd')
+const inquirer = require('inquirer')
+const chalk = require('chalk')
+const Spinner = require('cli-spinner').Spinner
+const figlet = require('figlet')
+
+
+module.exports = {
+  getCurrentDirectoryBase: () => {
+    return path.basename(process.cwd())
+  },
+  directoryExists: (filePath) => {
+    try {
+      return fs.statSync(filePath).isDirectory()
+    } catch (err) {
+      return false
+    }
+  }
+}
+
+
+const spinner = new Spinner(chalk.bold.magenta('install %s'))
+spinner.setSpinnerString(18)
+
+const welcome = () => {
+  console.log(
+    chalk.yellow(
+      figlet.textSync('Angular Rails Starterkit', { horizontalLayout: 'full' })
+    )
+  )
+}
+
+const error = (err) => {
+  spinner.stop()
+  console.log(chalk.bold.red(err))
+  process.exit(-1)
+}
+
+const beforeInstall = () => {
+  cmd.get('ls client',
+    (err, data, stderr) => {
+      if (!data) {
+        install()
+      } else {
+        console.log(chalk.bold.red('Cilent folder already exist\r\n'))
+        const questions = [{
+          name: 'option',
+          type: 'list',
+          message: 'Are you sure wont rewrite client folder',
+          choices: [ 'yes', 'not']
+        }]
+
+        inquirer.prompt(questions).then(function (answers) {
+        	if (answers.option === 'yes') {
+            cmd.get('rm -rf client', install)
+          } else {
+            process.exit(1)
+          }
+        })
+      }
+    }
+  )
+}
+
+const afterInstall = () => {
+  console.log(chalk.blue('Installing packages for tooling via npm.'))
+  cmd.get('cd client/; npm i',
+    (err, data, stderr) => {
+      if (err) {
+        error(err)
+      } else {
+        spinner.stop()
+        console.log(data)
+        console.log(chalk.green('Successfuly generated'))
+      }
+  })
+}
 
 const install = () => {
-  return Repository.clone('https://github.com/igorbezsmertnyi/angular-2-rails-starterkit', 'client')
-                   .then(repo => console.log(repo))
-                   .catch(e => console.log(e))
+  Repository.clone('https://github.com/igorbezsmertnyi/starterkit-content', 'client')
+   .then(repo => {
+     spinner.start()
+     console.log(chalk.blue('Successfully initialized git.'))
+     afterInstall()
+   })
+   .catch(err => error(err))
+}
+
+const herokuProcfiles = () => {
+  fs.writeFile("./Procfile", "web: bundle exec rails server -p $PORT", (err) => {
+    if (err) error(err)
+    console.log(chalk.green('Procfile - created'))
+  })
+
+  fs.writeFile("./Procfile.prod", "angular-assets: sh -c 'cd client/ ; ng build'", (err) => {
+    if (err) error(err)
+    console.log(chalk.green('Procfile.prod - created'))
+  })
+
+  fs.writeFile("./Procfile.dev", "angular-assets: sh -c 'cd client/ ; ng build --dev --watch'\r\nrails-server: rails s -p 3000", (err) => {
+    if (err) error(err)
+    console.log(chalk.green('Procfile.dev - created'))
+  })
 }
 
 Program
-  .option('-i, --install [name]', 'Instalation')
-  .option('-H, --heroku', 'Heroku deployment')
+  .option('-g, --generate', 'Generate files')
+  .option('-h, --help', 'Help')
   .parse(process.argv)
 
-if (Program.install) {
-  console.log('install %s app', Program.install)
-  install()
+if (Program.generate) {
+  welcome()
+  console.log(chalk.bold.magenta('Generate files'))
+  beforeInstall()
+  herokuProcfiles()
 }
-if (Program.heroku) console.log('heroku')
